44b1f170443f8bc360416f11dd251e966b7e3c6f
Kevin McHugh <kev@kevinmchugh.me>
2019-02-26 18:09:40 -0600

    Adds more game states

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,14 +18,29 @@ class Game < ApplicationRecord
   aasm column: :state do
     state :created, initial: true
     state :started
+    state :voting_opened
+    state :final_question_opened
+    state :final_voting_opened
     state :finished
 
     event :start do
       transitions from: :requested, to: :started
     end
 
+    event :open_voting do
+      transitions from: :started, to: :voting_opened
+    end
+
+    event :open_final_question do
+      transitions from: :voting_opened, to: :final_question_opened
+    end
+
+    event :open_final_voting do
+      transitions from: :final_question_opened, to: :final_voting_opened
+    end
+
     event :finish do
-      transitions from: :started, to: :finished
+      transitions from: :final_voting_opened, to: :finished
     end
   end
 
