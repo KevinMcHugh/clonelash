b60aa0513968694bd3260fa73e27a70e1fc5c203
Kevin McHugh <kev@kevinmchugh.me>
2019-03-01 07:57:25 -0600

    adds public files created by npm installing???

---
+++ public/precache-manifest.d83ff494a96005ee235ae033547ec9e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d2d3ed977f8487993861",
+    "url": "/static/js/main.d2d3ed97.chunk.js"
+  },
+  {
+    "revision": "63533011dd9e22859160",
+    "url": "/static/js/2.63533011.chunk.js"
+  },
+  {
+    "revision": "d2d3ed977f8487993861",
+    "url": "/static/css/main.ebdcc144.chunk.css"
+  },
+  {
+    "revision": "a89e820ff9b080519819699cbfab3e6c",
+    "url": "/index.html"
+  }
+];
