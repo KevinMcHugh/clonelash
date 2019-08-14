62cd570e6c23872052f053382892758f17c84fcf
Kevin McHugh <kev@kevinmchugh.me>
2019-08-13 19:33:51 -0500

    Trying to make actioncable accessible.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "ws://clonelash.herokuapp.com.herokuapp.com/cable"
 end
