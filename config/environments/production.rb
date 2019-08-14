5dbebf97f6c04c98c03b274f063db35ef501233b
Kevin McHugh <kev@kevinmchugh.me>
2019-08-13 21:24:31 -0500

    more fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.web_socket_server_url = "ws://clonelash.herokuapp.com.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://clonelash.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ 'http://clonelash.herokuapp.com', 'https://clonelash.herokuapp.com' ]
 end
