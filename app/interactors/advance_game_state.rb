24dc28b23625b6f20b7de49e0b1d1cf07dc96d25
Kevin McHugh <kev@kevinmchugh.me>
2019-08-29 15:36:20 -0500

    Adds more prompts

--- app/interactors/advance_game_state.rb
+++ app/interactors/advance_game_state.rb
@@ -5,7 +5,9 @@ class AdvanceGameState
     game = context.game
 
     if game.voting_opened?
-      final_prompt = GamePrompt.create(prompt: Prompt.find_by(for_all_players: true),
+      used_game_prompt_ids = game.game_prompts.pluck(:id)
+
+      final_prompt = GamePrompt.create(prompt: Prompt.where.not(id: used_game_prompt_ids).first,
                                        game: game,
                                        final_question: true,
                                        state: :accepting_answers)
@@ -16,6 +18,7 @@ class AdvanceGameState
         # wait actually this is gonna kind suck it's very slow.
         PlayerChannel.broadcast_to(player, response.to_socket_json)
       end
+      PlayerChannel.broadcast_to(game.players.find_by(admin: true), Response.new(game_prompt: final_prompt))
       game.open_final_question!
     elsif game.final_voting_opened?
       game.finish!
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-[
-  { text: "A new name for armpit hair:", format: :new_name },
-  { text: "A celebrity you would like to eat, and the dish you'd eat them with:", format: :misc },
-  { text: 'What two words would passengers never want to hear a pilot say?', format: :misc},
-  { text: 'You would never go on a roller coaster called "BLANK"', format: :fill_in_the_blank},
-  { text: 'The secret to a happy life', format: :misc},
-  { text: 'If a winning coach gets Gatorade dumped on his head, what should get dumped on the losing coach?', format: :misc},
-  { text: 'Everyone knows that monkeys hate "BLANK"', format: :fill_in_the_blank},
-  { text: 'The biggest downside to living in Hell', format: :misc},
-  { text: "Jesus's REAL last words", format: :quote},
-  { text: 'If a genie gives you three wishes, the best things to wish for are: 1) a billion dollars, 2) eternal life, and 3) <BLANK>', format: :fill_in_the_blank},
-  { text: 'What an alarm clock could say that would wake you right up', format: :quote },
-  { text: 'The biggest issue facing the town of Margaritaville', format: :misc, for_all_players: true }
-].each do |hash|
-  Prompt.create(hash)
+# [
+#   { text: "A new name for armpit hair:", format: :new_name },
+#   { text: "A celebrity you would like to eat, and the dish you'd eat them with:", format: :misc },
+#   { text: 'What two words would passengers never want to hear a pilot say?', format: :misc},
+#   { text: 'You would never go on a roller coaster called "BLANK"', format: :fill_in_the_blank},
+#   { text: 'The secret to a happy life', format: :misc},
+#   { text: 'If a winning coach gets Gatorade dumped on his head, what should get dumped on the losing coach?', format: :misc},
+#   { text: 'Everyone knows that monkeys hate "BLANK"', format: :fill_in_the_blank},
+#   { text: 'The biggest downside to living in Hell', format: :misc},
+#   { text: "Jesus's REAL last words", format: :quote},
+#   { text: 'If a genie gives you three wishes, the best things to wish for are: 1) a billion dollars, 2) eternal life, and 3) <BLANK>', format: :fill_in_the_blank},
+#   { text: 'What an alarm clock could say that would wake you right up', format: :quote },
+#   { text: 'The biggest issue facing the town of Margaritaville', format: :misc, for_all_players: true }
+# ].each do |hash|
+#   Prompt.create(hash)
+# end
+
+File.read('db/prompts').lines.each do |line|
+  Prompt.create(text: line)
 end
