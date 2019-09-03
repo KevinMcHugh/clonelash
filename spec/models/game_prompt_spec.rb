3e817eb7609b7782304818e70483c7cbcc7d1168
Kevin McHugh <kev@kevinmchugh.me>
2019-09-03 15:52:00 -0500

    Adds specs for game prompt finally

--- spec/models/game_prompt_spec.rb
+++ spec/models/game_prompt_spec.rb
@@ -14,14 +14,15 @@
 require 'rails_helper'
 
 RSpec.describe GamePrompt do
-  describe '#all_votes_received?' do
-    let(:game) { Game.create }
-    let(:prompt) { Prompt.create }
-    let(:game_prompt) { GamePrompt.create(game: game, prompt: prompt) }
-    let(:players) { 4.times.map { |i| Player.create(name: i, game: game)} }
+  let(:game) { Game.create }
+  let(:prompt) { Prompt.create }
+  let(:game_prompt) { described_class.create(game: game, prompt: prompt) }
+  let(:players) { 4.times.map { |i| Player.create(name: i, game: game)} }
 
-    let!(:response_1) { Response.create(game_prompt: game_prompt, player: players[0]) }
-    let!(:response_2) { Response.create(game_prompt: game_prompt, player: players[1]) }
+  let!(:response_1) { Response.create(game_prompt: game_prompt, player: players[0]) }
+  let!(:response_2) { Response.create(game_prompt: game_prompt, player: players[1]) }
+
+  describe '#all_votes_received?' do
     let(:vote_1) { Vote.create(game_prompt: game_prompt, response: response_1)}
 
     subject { game_prompt.all_votes_received? }
@@ -47,6 +48,74 @@ RSpec.describe GamePrompt do
   end
 
   describe '#as_json' do
+    subject { game_prompt.as_json(player: player) }
 
+    context 'player is provided' do
+      let(:player) { Player.create(game: game, admin: admin) }
+
+      context 'player is an admin' do
+        let(:admin) { true }
+        it 'says no responses are selectable' do
+          expect(subject[:responses].map { |r| r[:selectable] }).to eq([false,false])
+        end
+      end
+
+      context 'player is not an admin' do
+        let(:admin) { false }
+
+        context 'player has not voted' do
+          context 'final question' do
+            let!(:player_response) { Response.create(player: player, game_prompt: game_prompt)}
+            let(:game_prompt) { described_class.create(game: game, prompt: prompt, final_question: true) }
+            it 'allows responses by everyone other than the player' do
+              expect(subject[:responses].reject { |r| r[:id] == player_response.id }.map{|r| r[:selectable]}).to eq([true,true])
+              expect(subject[:responses].find { |r| r[:id] == player_response.id }[:selectable]).to eq(false)
+            end
+          end
+          context 'not final question' do
+            context 'player has responded' do
+              let!(:player_response) { Response.create(player: player, game_prompt: game_prompt)}
+
+              it 'says no responses are selectable' do
+                expect(subject[:responses].map { |r| r[:selectable] }).to eq([false,false,false])
+              end
+            end
+            context 'player has not responded' do
+              it 'says all responses are selectable' do
+                expect(subject[:responses].map { |r| r[:selectable] }).to eq([false,false])
+              end
+            end
+          end
+        end
+
+        context 'player has voted' do
+          # TODO this is the stuff that's actually broken I think
+          let!(:player_vote) { Vote.create(game_prompt: game_prompt, player: player, response: response_1) }
+          context 'final question' do
+            let!(:player_response) { Response.create(player: player, game_prompt: game_prompt)}
+            let(:game_prompt) { described_class.create(game: game, prompt: prompt, final_question: true) }
+            it 'says no responses are selectable' do
+              expect(subject[:responses].map{|r| r[:selectable]}).to eq([false,false,false])
+            end
+          end
+          context 'not final question' do
+            context 'player has not responded' do
+              it 'says all responses are selectable' do
+                expect(subject[:responses].map { |r| r[:selectable] }).to eq([false,false])
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context 'player is not provided' do
+      # TODO: this essentially represents the audience member state. This
+      # will have to change to support that behavior - once that behavior exists.
+      let(:player) { nil }
+      it 'says no responses are selectable' do
+        expect(subject[:responses].map { |r| r[:selectable] }).to eq([false,false])
+      end
+    end
   end
 end
