ed68ac7c4829ac499f4b2b7d25c953550431942a
Kevin McHugh <kev@kevinmchugh.me>
2019-09-04 11:42:10 -0500

    Animation tweaks/fixes

--- client/src/Players.js
+++ client/src/Players.js
@@ -30,7 +30,7 @@ class Players extends Component {
         let oldPlayer = players[playerIndex]
         if (oldPlayer.score != response.score) {
           console.log(response.name + " score updated")
-          _.merge(response, {'scoreUpdated': true})
+          response.scoreUpdated = oldPlayer.scoreUpdated ? oldPlayer.scoreUpdated + 1 : 1
         }
         players[playerIndex] = response
       } else {
@@ -63,7 +63,8 @@ class Players extends Component {
               className += " Player-winner"
             }
             if (player.scoreUpdated) {
-              className += " Player-score-updated"
+              console.log(player.name + " " + player.scoreUpdated)
+              className += " Player-score-updated-" + player.scoreUpdated % 2
             }
             return (
               <div key={player.id} className={className}>
