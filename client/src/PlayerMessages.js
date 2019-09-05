fcbdef2293a3fb4c9f76868842e44ed72b757f59
Kevin McHugh <kev@kevinmchugh.me>
2019-09-05 16:48:07 -0500

    Minor beautification

--- client/src/PlayerMessages.js
+++ client/src/PlayerMessages.js
@@ -6,13 +6,12 @@ class PlayerMessages extends Component {
   render() {
     const playerId = this.props.playerId
     const winnerNames = this.props.game.winners.map(winner => { return winner.name})
-    // TODO make everyone vote on responses simultaneously....
     if (this.props.game.state === 'started' || this.props.game.state === 'final_question_opened') {
       return (<Prompt playerId={playerId}/>)
     } else if (this.props.game.state === 'voting_opened' || this.props.game.state === 'final_voting_opened') {
       return (<Vote playerId={playerId} game={this.props.game}/>)
     } else if (this.props.game.state === 'finished') {
-      return "Congrats to the winner, " + winnerNames
+      return (winnerNames.length > 1 ? "Congrats to the winners, " : "Congrats to the winner, ") + winnerNames
     } else {
       return "Game will start soon...Maybe a join code goes here."
     }
